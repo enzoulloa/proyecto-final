7c0ee66bbd098e677c1db554b4e4a43c1fc77de7
mariano48 <marianosolis423@gmail.com>
2022-10-12 16:23:16 -0300

    package.json update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import store from "./redux/store.js";
+import App from "./routes/App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-)
+  </Provider>
+);
