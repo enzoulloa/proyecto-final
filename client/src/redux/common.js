aa9558cf8a247ae2d91e4fb4ccc3bf06f4361e2e
mariano48 <marianosolis423@gmail.com>
2022-10-13 15:27:23 -0300

    componente nav

---
+++ client/src/redux/common.js
@@ -0,0 +1,3 @@
+export const GET_OWNERSHIPS = "GET_OWNERSHIPS";
+export const LOADING = "LOADING";
+export const GET_USERS = "GET_USERS";
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,3 +1,5 @@
+import { GET_OWNERSHIPS, GET_USERS, LOADING } from "./common";
+
 const initialState = {
   properties: [],
   propertyDetail: [],
@@ -8,6 +10,30 @@ const initialState = {
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
+    case GET_OWNERSHIPS:
+      return {
+        ...state,
+        properties: action.payload,
+        loading: false,
+        error: false,
+        response: null,
+      };
+
+    case GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+        error: false,
+        response: null,
+      };
+
+    case LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+
     default:
       return state;
   }
