eeec67a70a518b579b696aededdef7c1c3ccee85
Franco Camus <francocamuspp@gmail.com>
2022-10-15 04:20:39 -0300

    paginado creado con React Paginate

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -5,17 +5,19 @@ import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
 import Error from "../components/Error"
 import Loading from "../components/Loading"
+import Paginated from "../components/Paginate";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/paginated" element={<Paginated />} />
         <Route path="/about" element={<About />} />
         <Route path="/nav" element={<NavBar />} />
         <Route path="/search" element={<SearchBar />} />
         <Route path="/error" element={<Error />} />
         <Route path="/loading" element={<Loading />} />
+        <Route path="/" />
       </Routes>
     </BrowserRouter>
   );
