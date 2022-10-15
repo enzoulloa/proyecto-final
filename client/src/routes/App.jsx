9b11251478c024eb379aa4e8dead371ef205b98b
mariano48 <marianosolis423@gmail.com>
2022-10-14 22:55:38 -0300

    integracion de componentes

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -1,21 +1,16 @@
 import About from "../components/About";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "../components/NavBar";
-import SearchBar from "../components/SearchBar";
-import Error from "../components/Error"
-import Loading from "../components/Loading"
+import Home from "../components/Home/Home";
+import Listing from "../components/Listing/Listing";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/nav" element={<NavBar />} />
-        <Route path="/search" element={<SearchBar />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="/loading" element={<Loading />} />
+        <Route path="/listings" element={<Listing />} />
       </Routes>
     </BrowserRouter>
   );
