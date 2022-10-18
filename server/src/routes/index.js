f24bd195f0c1df2bee3848dd40bfdbc24d8d8d2a
ElChetoMDQ97 <braianher5@gmail.com>
2022-10-17 21:17:17 -0300

    login y logout

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,11 +5,13 @@ const ownerships = require('./ownerships.js')
 const deleteUsers = require('./deleteUser.js')
 const deleteOwnerships = require('./deleteOwnership.js')
 const login = require('./login.js')
+const logout = require('./logout.js')
 
 api.use('/users', users)
 api.use('/ownerships', ownerships)
 api.use('/deleteUsers', deleteUsers)
 api.use('/deleteOwnerships', deleteOwnerships)
 api.use('/login', login)
+api.use('/logout', logout)
 
 module.exports = api;
