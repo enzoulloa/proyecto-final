41ccc053a4d1ca328146551640a486156ca75a47
Franco Camus <francocamuspp@gmail.com>
2022-10-21 15:45:02 -0300

    Componenete Review en proceso

---
+++ client/src/components/Review/Review.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import "./review.scss";
+import {postReview} from "../redux/actions.js";
+
+export default function Review() {
+    const dispatch = useDispatch()
+    const [review, setReview] = useState("")
+    
+    const handleChange = (e) => {
+        e.preventDefault()
+        setReview({[e.target.name]: e.target.value})
+        console.log(review)
+    }
+
+    const sendReview = (e) => {
+        e.preventDefault()
+        dispatch(()=>postReview())
+    }
+
+    return (
+        <div className="div-detail">         
+                <strong className="strong-review">Deja tu comentario!</strong>
+            <div>
+                <textarea
+                    name="review"
+                    className="textarea-review"
+                    onChange={handleChange}
+                    value={review.review}
+                    placeholder="Escribe...">
+                </textarea>
+            </div>
+            <div>
+                <button onClick={sendReview}>Enviar comentario</button>
+            </div>
+        </div>
+    )
+}
