94f134d4105d2be7550838285d096f91f57ef55d
mariano48 <91891330+mariano48@users.noreply.github.com>
2022-10-24 16:07:24 -0300

    Update common.js

--- client/src/redux/common.js
+++ client/src/redux/common.js
@@ -11,10 +11,7 @@ export const FILTER_BY = "FILTER_BY";
 export const LOGIN_USER = "LOGIN_USER";
 export const EXIT_SESSION = "EXIT_SESSION";
 export const LOGIN_USER_AUTH0 = "LOGIN_USER_AUTH0";
-<<<<<<< HEAD
-=======
 export const USER_STATUS = "USER_STATUS";
->>>>>>> 6a7652dd50d0b8bd1c4b33a83d45d69038545e94
 // export const FILTER_BY_TYPE = "FILTER_BY_TYPE";
 export const ORDER_OWNERSHIPS = "ORDER_OWNERSHIPS";
 export const GET_STATUS_LOGIN = "GET_STATUS_LOGIN";
