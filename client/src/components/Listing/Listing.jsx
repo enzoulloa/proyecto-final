05994854d626f99585beed109d88a46f3f6c39f0
Bezagus <agustindavidbenitez1@gmail.com>
2022-10-28 15:10:00 -0300

    cambios

--- client/src/components/Listing/Listing.jsx
+++ client/src/components/Listing/Listing.jsx
@@ -24,6 +24,7 @@ export default function Listing() {
     if (ownerships.length === 0){
     dispatch(GetOwnerships());
     dispatch(userFavorite())
+    }
   }, [dispatch]);
 
   useEffect(() => {
