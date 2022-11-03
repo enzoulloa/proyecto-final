c29f394634092357ed15e27f6910d5dac3fb7ee5
mariano48 <marianosolis423@gmail.com>
2022-11-03 01:07:29 -0300

    userSales styles

--- client/src/components/UserOwnerships.jsx
+++ client/src/components/UserOwnerships.jsx
@@ -1,41 +1,15 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getSales, mercadoPagoId } from "../redux/actions";
-import { Table } from "antd";
-import "./PaymentStatus.scss";
-import { columnSales } from "./UserDetails/common";
-
-//---------------------------------------------------------
-//Ver si lo renderizamos aca, o como cards en /informacion
-//---------------------------------------------------------
+import "./UserOwnership.scss";
 
 export default function UserOwnerships() {
   const dispatch = useDispatch();
-  // window.location.search()
   const ownershipId = window.location.search.split("=")[1]
     ? window.location.search.split("=")[1].split("&")[0]
     : null;
-  console.log(ownershipId);
-  // const idUser = window.location.search.split('=')[3];
-  // console.log(idUser);
   const userId = JSON.parse(window.localStorage.getItem("UserLogin")).id;
-  console.log(userId);
-  // const userId = useParams();
-  // let paymentId = useSelector(state => state.paymentId);
   const userSales = useSelector((state) => state.userSales);
-  console.log(userSales);
-  // let paymentStatus = useSelector(state => state.paymentStatus);
-  // let paymentStatus = {
-  //     status: 'approved'
-  // };
-
-  // useEffect(() => {
-  //     if(paymentId) {
-  //         console.log(paymentId);
-  //         dispatch(mercadoPagoPayment(paymentId));
-  //     }
-  // }, [paymentId]);
 
   useEffect(() => {
     if (ownershipId) {
@@ -44,32 +18,43 @@ export default function UserOwnerships() {
     dispatch(getSales(userId));
   }, [dispatch, ownershipId]);
 
-  // useEffect(() => {
-  //     // console.log('entro al efecto');
-  //     // dispatch(getUserId(idUser));
-  //     // dispatch(mercadoPagoId(ownershipId));
-  //     dispatch(getSales(userId));
-  // }, [dispatch]);
-
   return (
-    <div>
-      {/* {
-                userSales.length && userSales.map(sale => (
-                    <div>
-                        <div>{}</div>
-                        <Cards 
-                            images={}
-                            name={}
-                            location={}
-                            price={}
-                            rooms={}
-                            type={}
-                            id={}
-                        />
-                    </div>
-                ))
-            } */}
-      <Table dataSource={userSales} columns={columnSales} />
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        marginTop: "20px",
+      }}
+    >
+      <h2 style={{ color: "#f1f1f0", alignSelf: "center" }}>
+        Listado de señas realizadas
+      </h2>
+      {userSales &&
+        userSales.map((s, i) => {
+          return (
+            <div class="wrap-collabsible">
+              <input id="collapsible" class="toggle" type="checkbox" />
+              <label for="collapsible" class="lbl-toggle">
+                {s.Ownerships[0].name}
+              </label>
+              <div class="collapsible-content">
+                <div class="content-inner">
+                  <p>
+                    Id de pago: <b>{s.paymentId}</b>
+                  </p>
+                  <p>
+                    Estado de la seña:{" "}
+                    <b>{s.state === "approved" ? "Aprobado" : "En proceso"}</b>
+                  </p>
+                  <p>
+                    Valor de la seña: <b>${s.Ownerships[0].price * 0.1}</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
